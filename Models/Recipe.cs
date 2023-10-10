f7ae584938f74ec1a51158f1bbb496c98a38c498
Rika Saito <wind10cloud@gmail.com>
2023-10-10 10:43:54 +0900

    deleted footer

--- Models/Recipe.cs
+++ Models/Recipe.cs
@@ -1,12 +1,29 @@
-﻿namespace CookingRecipes.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CookingRecipes.Models
 {
     public class Recipe
     {
         public int Id { get; set; }
+        
+        [DataType(DataType.Text)]
+        [Required]
         public string Name { get; set; }
+
+        [DataType(DataType.Text)]
         public string? Description { get; set; }
+
+        [DataType(DataType.PhoneNumber)]
+        [Required]
         public int Time { get; set; }
+
+
+        [DataType(DataType.Text)]
+        [Required]
         public string Ingredients { get; set; }
+
+        [DataType(DataType.Text)]
+        [Required]
         public string Directions { get; set; }
     }
 }
